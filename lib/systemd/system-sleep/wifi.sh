748da9c34a9170c8becac18b4e0641d56e219d71
Hugh Greenberg <hugegreenbug@gmail.com>
2016-04-28 23:04:26 -0600

    Changed wifi sleep script

--- lib/systemd/system-sleep/wifi.sh
+++ lib/systemd/system-sleep/wifi.sh
@@ -2,12 +2,21 @@
 # File: "/lib/systemd/system-sleep/wifi.sh".
 
 case $1/$2 in
-    pre/*)
-	/sbin/rmmod ath9k
-	;;
     post/*)
-	/sbin/modprobe ath9k
-	systemctl restart NetworkManager
-	;;
+        for i in `seq 1 5`
+        do
+            rmmod ath9k ath9k_common ath9k_hw ath mac80211 cfg80211
+	    modprobe ath9k
+            sleep 1
+            systemctl restart wpa_supplicant
+            systemctl restart NetworkManager
+            sleep 2
+            WIFI=`nmcli d | grep wifi`
+            if [ -n "$WIFI" ]
+            then
+                break
+            fi
+        done
+        ;;
 esac
 exit 0
